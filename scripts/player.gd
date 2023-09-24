93cfe9962e5566e17cf594bd2622bc572caadcf9
Matt Cribbs <mcribbs@caesars.com>
2023-09-24 11:38:58 -0400

    Lesson 1: Add player with animation
    https://www.youtube.com/watch?v=pBoXqW4RykE&list=PL3cGrGHvkwn0zoGLoGorwvGj6dHCjLaGd&index=1

---
+++ scripts/player.gd
@@ -0,0 +1,57 @@
+extends CharacterBody2D
+
+enum DIRECTION {RIGHT, LEFT, UP, DOWN}
+
+const SPEED = 100
+var current_direction
+
+func _ready():
+	$AnimatedSprite2D.play("front_idle")
+
+func _physics_process(delta):
+	player_movement(delta)
+
+func player_movement(delta):
+	
+	if Input.is_action_pressed("ui_right"):
+		current_direction = DIRECTION.RIGHT
+		play_move_amination(true)
+		velocity.x = SPEED
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		current_direction = DIRECTION.LEFT
+		play_move_amination(true)
+		velocity.x = -SPEED
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_down"):
+		current_direction = DIRECTION.DOWN
+		play_move_amination(true)
+		velocity.x = 0
+		velocity.y = SPEED
+	elif Input.is_action_pressed("ui_up"):
+		current_direction = DIRECTION.UP
+		play_move_amination(true)
+		velocity.x = 0
+		velocity.y = -SPEED
+	else:
+		play_move_amination(false)
+		velocity.x = 0
+		velocity.y = 0
+		
+	move_and_slide()
+
+func play_move_amination(is_moving):
+	var dir = current_direction
+	var anim = $AnimatedSprite2D
+	
+	match dir:
+		DIRECTION.RIGHT:
+			anim.flip_h = false
+			is_moving if anim.play("side_walk") else anim.play("side_idle")
+		DIRECTION.LEFT:
+			anim.flip_h = true
+			is_moving if anim.play("side_walk") else anim.play("side_idle")
+		DIRECTION.UP:
+			is_moving if anim.play("back_walk") else anim.play("back_idle")
+		DIRECTION.DOWN:
+			is_moving if anim.play("front_walk") else anim.play("front_idle")
